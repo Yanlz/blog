8640fea8daa7c92c6d361f38392e1f41fb33ad56
yanliangzhao <584598008@qq.com>
2015-12-28 19:06:21 +0800

    modify pom.xml

--- blog-app/srv/main/java/org/yanlz/blog/application.java
+++ blog-app/srv/main/java/org/yanlz/blog/application.java
@@ -1,7 +1,15 @@
 package org.yanlz.blog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by yanliangzhao on 15/12/28.
  */
+@SpringBootApplication
 public class application {
+    public static void main(String[] args){
+        SpringApplication springApplication = new SpringApplication(application.class);
+        springApplication.run(args);
+    }
 }
