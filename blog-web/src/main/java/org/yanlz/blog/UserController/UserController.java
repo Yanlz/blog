f753bca21cc2c60184f7de56363a0e9956acd204
yanliangzhao <584598008@qq.com>
2015-12-30 12:03:01 +0800

    velocicty view

--- blog-web/src/main/java/org/yanlz/blog/UserController/UserController.java
+++ blog-web/src/main/java/org/yanlz/blog/UserController/UserController.java
@@ -2,12 +2,15 @@ package org.yanlz.blog.UserController;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.yanlz.blog.db.tables.interfaces.IUserInfo;
 import org.yanlz.blog.user.UserRequest;
 import org.yanlz.blog.user.service.IUserService;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by yanliangzhao on 15/12/29.
@@ -26,11 +29,13 @@ public class UserController {
      */
     @Resource
     IUserService iUserService;
-
     @RequestMapping(value = "/user/add" , method = RequestMethod.GET)
-    public String addUser(UserRequest userRequest){
-
+    @ResponseBody
+    public IUserInfo addUser(UserRequest userRequest){
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("1","hello");
+        map.put("2","world");
         IUserInfo userInfo = iUserService.insertUser(userRequest);
-        return userInfo.toString();
+        return userInfo;
     }
 }
--- blog-web/src/main/java/org/yanlz/blog/testController.java
+++ blog-web/src/main/java/org/yanlz/blog/testController.java
@@ -1,7 +1,19 @@
 package org.yanlz.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by yanliangzhao on 15/12/29.
  */
+@Controller
+@RequestMapping("/test1")
 public class testController {
+
+    @RequestMapping("/veloctity")
+    public String velocityTest(Model model){
+        model.addAttribute("test","hell velocity");
+        return "wellcome";
+    }
 }
